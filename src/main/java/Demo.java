411bf3b449b67cc12b7882c9706569bdfa15edf0
kodi-l <kodilangstoninfo@gmail.com>
2025-06-25 10:52:52 -0500

    Update Demo.java

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+public class Demo {
+
+    public static void main(String[] args) {
+        // Reading from System.in
+        Scanner reader = new Scanner(System.in);  
+
+        System.out.println("Enter side 1: ");
+        int side_1 = reader.nextInt();
+
+        System.out.println("Enter side 2: ");
+        int side_2 = reader.nextInt();
+
+        System.out.println("Enter side 3: ");
+        int side_3 = reader.nextInt();
+
+        if (isTriangle(side_1, side_2, side_3)) {
+            System.out.println("This is a triangle.");
+        } else {
+            System.out.println("This is not a triangle.");
+        }
+
+        reader.close();
+    }
+
+    public static boolean isTriangle(double a, double b, double c) {
+        if ((a + b > c) &&
+            (a + c > b) &&
+            (b + c > a)) {
+            return true;
+        }
+        return false;
+    }
+}
