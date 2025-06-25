fb72965ca419d7bad8e1868b51cc29ec6af03938
kodi-l <kodilangstoninfo@gmail.com>
2025-06-25 10:39:21 -0500

    Update DemoTest.java

--- DemoTest.java
+++ DemoTest.java
@@ -1,2 +1,16 @@
-// Placeholder for JUnit tests
-// Will test isTriangle with boundary and normal values
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DemoTest {
+
+    @Test
+    public void testValidTriangle() {
+        // Placeholder for a test case
+    }
+
+    @Test
+    public void testInvalidTriangle() {
+        // Placeholder for a test case
+    }
+
+}
